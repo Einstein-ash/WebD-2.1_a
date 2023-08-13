47b4310dd2a36eaff120ea106e7214a7db2a4db9
Ashwani <ashwanisingh46572@gmail.com>
2023-08-14 01:44:46 +0530

    added footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,6 +26,7 @@ export default function Home() {
         </div>
             <TabLinks />
             <UpcomingWorkshops />
+            
             {/* <Contact /> */}
 
             <Footer />
