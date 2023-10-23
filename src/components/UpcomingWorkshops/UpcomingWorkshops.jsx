4faa6708c75bdb60b99801f4d94a5002c925fb8d
Ashwani <ashwanisingh46572@gmail.com>
2023-10-24 00:11:26 +0530

    updating for embedded workshop 2k23

--- src/components/UpcomingWorkshops/UpcomingWorkshops.jsx
+++ src/components/UpcomingWorkshops/UpcomingWorkshops.jsx
@@ -1,6 +1,6 @@
 import "../../assets/css/UpcomingWorkshop.css";
 import React from "react";
-import pic1 from "../../assets/images/dip_fee.png";
+import pic1 from "../../assets/images/2nd_yr_induction.png";
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
 
@@ -15,7 +15,7 @@ const UpcomingWorkshops = () => {
       <div className="module_border_wrap ">
         <div className="module">
           <img alt="Upcoming workshop" className="shadow-sm shadow-white " src={pic1}></img>
-          <AwesomeButton size="medium" type="secondary" href="https://docs.google.com/forms/d/e/1FAIpQLScYHejNj8lWYlgiqxrZlVRqbA56Jv9vypucfDMssFy3_JOJnw/viewform">
+          <AwesomeButton size="medium" type="secondary" href="https://docs.google.com/forms/d/e/1FAIpQLSdYLlQgMdgqTbKGmr2VmpWe--SErdeXw7T53BsT4Uf0cmgNyg/viewform?usp=sf_link">
             Apply Now
           </AwesomeButton>
         </div>
